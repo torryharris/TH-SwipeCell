f1fc3e3d761785d067edd51fd4957fd701a8590f
Loganathan <loganathan_balakrishnan@thbs.com>
2014-11-03 12:03:25 +0530

    Initial Commit

---
+++ THSwipeCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  THSwipeCell
+//
+//  Created by Loganathan on 03/11/14.
+//  Copyright (c) 2014 Torry Harris Business Soutions Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
